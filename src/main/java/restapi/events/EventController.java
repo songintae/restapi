9bed8d3d76c34eedd238eb990d0e5842fd9142ce
intaesong <intaesong@woowahan.com>
2018-11-12 23:00:19 +0900

    Event 상세조회 구현

--- src/main/java/restapi/events/EventController.java
+++ src/main/java/restapi/events/EventController.java
@@ -4,13 +4,10 @@ package restapi.events;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
-import org.springframework.http.HttpHeaders;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -18,7 +15,7 @@ import java.net.URI;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
     @Autowired
     private EventRepository eventRepository;
@@ -49,5 +46,16 @@ public class EventController {
         eventResource.add(linkTo(EventController.class).slash(savedEvent.getId()).withRel("update"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(locationUri).body(eventResource);
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity get(@PathVariable Integer eventId) {
+        Event event = eventRepository.findById(eventId).orElseThrow(RuntimeException::new);
+
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).slash(event.getId()).withRel("update"));
+        eventResource.add(linkTo(EventController.class).withRel("events"));
+        eventResource.add(new Link("/docs/index.html#resources-events-read").withRel("profile"));
+        return ResponseEntity.ok(eventResource);
     }
 }
--- src/test/java/restapi/events/EventControllerTest.java
+++ src/test/java/restapi/events/EventControllerTest.java
@@ -18,8 +18,11 @@ import java.time.LocalDateTime;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,6 +41,9 @@ public class EventControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
+    @Autowired
+    private EventRepository eventRepository;
 
     @Test
     public void Event_생성_테스트() throws Exception {
@@ -139,5 +145,49 @@ public class EventControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
         ;
+    }
+
+    @Test
+    public void Event_조회_테스트() throws Exception {
+        // given
+        Event event = Event.builder()
+                .name("Spring rest api")
+                .description("REST 다운 REST API 작성")
+                .location("korea")
+                .date(LocalDateTime.of(2019, 11, 10, 9, 30))
+                .price(0)
+                .build();
+
+        Event savedEvent = eventRepository.save(event);
+
+        // when & then
+        mockMvc.perform(get("/api/events/{eventId}", event.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(savedEvent.getId()))
+                .andExpect(jsonPath("name").value(event.getName()))
+                .andExpect(jsonPath("location").value(event.getLocation()))
+                .andExpect(jsonPath("_links.update").hasJsonPath())
+                .andExpect(jsonPath("_links.events").hasJsonPath())
+                .andExpect(jsonPath("_links.profile").hasJsonPath())
+                .andDo(document("read-events",
+                        links(
+                                linkWithRel("self").description("link to self"),
+                                linkWithRel("events").description("link to events"),
+                                linkWithRel("update").description("link to update"),
+                                linkWithRel("profile").description("link to profile")
+                        ),
+                        pathParameters(
+                          parameterWithName("eventId").description("조회 대상 Event Id")
+                        ),
+                        relaxedResponseFields(
+                                fieldWithPath("name").description("Event 이름"),
+                                fieldWithPath("description").description("Event 설명"),
+                                fieldWithPath("location").description("Event 지역"),
+                                fieldWithPath("date").description("Event 일자"),
+                                fieldWithPath("price").description("Event 가격"),
+                                fieldWithPath("free").description("Event 무료 여부"),
+                                fieldWithPath("online").description("Event 온라인 여부")))
+                )
+                ;
     }
 }
